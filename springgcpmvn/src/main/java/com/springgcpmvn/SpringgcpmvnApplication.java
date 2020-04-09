ae4fe7ccc1b7bde8cb5adf5788eee1ea742059cf
yahiaDev <yahia.ammar.info@gmail.com>
2020-04-09 11:39:59 +0200

    project init

---
+++ springgcpmvn/src/main/java/com/springgcpmvn/SpringgcpmvnApplication.java
@@ -0,0 +1,13 @@
+package com.springgcpmvn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringgcpmvnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringgcpmvnApplication.class, args);
+	}
+
+}
